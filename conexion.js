c9d7b3f6b27f669641d61b9785bdedd85f3972ed
Juan Jesús Silvestre Chávez <134343358+TYisusK@users.noreply.github.com>
2023-08-14 07:35:10 -0600

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,38 +1,22 @@
-const Sequelize = require('sequelize');
-const modelos = require('./models/modelo');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const db = process.env.DB_MYSQL_REMOTO;
-const usuario = process.env.USUARIO_MYSQL_REMOTO;
-const password = process.env.PASSWORD_MYSQL_REMOTO;
-const host = process.env.HOST_MYSQL_REMOTO;
-const port = process.env.PORT_MYSQL_REMOTO;
+const dbURI = process.env.DB_MONGODB_URI;
 
-const conexion = new Sequelize(db, usuario, password, {
-  host: host,
-  port: port,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // You may need to adjust this based on your setup
-    },
-  },
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true, // Utilizar createIndexes en Mongoose 7.x
 });
 
-const { Usuario, Producto } = modelos(conexion);
+const db = mongoose.connection;
 
-// Sincroniza los modelos con la base de datos
-conexion.sync({ force: false })
-  .then(() => {
-    console.log('Conectado a MySQL en PlanetScale y sincronizado con modelos');
-  })
-  .catch((err) => {
-    console.log('Error al conectarse a MySQL:', err);
-  });
+db.on('error', (error) => {
+  console.error('Error al conectar a MongoDB:', error);
+});
 
-module.exports = {
-  Usuario: Usuario,
-  Producto: Producto,
-  conexion: conexion,
-};
+db.once('open', () => {
+  console.log('Conectado a MongoDB');
+});
+
+module.exports = db;
